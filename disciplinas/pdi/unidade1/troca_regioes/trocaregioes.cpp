59ac094aa1595aca406353f4e99d72a5bc2d8283
eldersantoss <elder_rn@hotmail.com>
2018-04-11 22:39:03 -0300

    .

--- disciplinas/pdi/unidade1/troca_regioes/trocaregioes.cpp
+++ disciplinas/pdi/unidade1/troca_regioes/trocaregioes.cpp
@@ -4,43 +4,44 @@
 using namespace cv;
 using namespace std;
 
-int main(int, char**){
-  Mat image;
-  CvPoint pmed;
+int main(int, char**)
+{
+    Mat image;
+    CvPoint pmed;
 
-  //Carregando imagem e armazenando no obejto image:
-  image = imread("biel.png",CV_LOAD_IMAGE_COLOR);
-  //Verificando carregamento:
-  if(!image.data)
-    cerr << "Imagem nao carregada! Tente novamente." << endl;
+    //Carregando imagem e armazenando no obejto image:
+    image = imread("biel.png",CV_LOAD_IMAGE_COLOR);
+    //Verificando carregamento:
+    if(!image.data)
+        cerr << "Imagem nao carregada! Tente novamente." << endl;
 
-  //Criando janela para exibição:
-  namedWindow("Regioes trocadas", WINDOW_AUTOSIZE);
+    //Criando janela para exibição:
+    namedWindow("Regioes trocadas", WINDOW_AUTOSIZE);
 
-  //Definindo falores centrais de cada coordenada:
-  pmed.x = image.rows/2;
-  pmed.y = image.cols/2;
+    //Definindo falores centrais de cada coordenada:
+    pmed.x = image.rows/2;
+    pmed.y = image.cols/2;
 
-  //Criando matriz auxiliar para armazenar quadrantes:
-  Mat temp(pmed.x, pmed.y, CV_8UC3);
+    //Criando matriz auxiliar para armazenar quadrantes:
+    Mat temp(pmed.x, pmed.y, CV_8UC3);
 
-  //Alterando posições dos quadrantes:
-  for(int i=0; i<pmed.x; i++)
-  {
-    for(int j=0; j<pmed.y; j++)
+    //Alterando posições dos quadrantes:
+    for(int i=0; i<pmed.x; i++)
     {
-        temp.at<Vec3b>(i, j) = image.at<Vec3b>(i,j);
-        image.at<Vec3b>(i,j) = image.at<Vec3b>(i+pmed.x,j+pmed.y);
-        image.at<Vec3b>(i+pmed.x,j+pmed.y) = temp.at<Vec3b>(i, j);
-        temp.at<Vec3b>(i, j) = image.at<Vec3b>(i+pmed.x,j);
-        image.at<Vec3b>(i+pmed.x,j) = image.at<Vec3b>(i,j+pmed.y);
-        image.at<Vec3b>(i,j+pmed.y) = temp.at<Vec3b>(i, j);
+        for(int j=0; j<pmed.y; j++)
+        {
+            temp.at<Vec3b>(i, j) = image.at<Vec3b>(i,j);
+            image.at<Vec3b>(i,j) = image.at<Vec3b>(i+pmed.x,j+pmed.y);
+            image.at<Vec3b>(i+pmed.x,j+pmed.y) = temp.at<Vec3b>(i, j);
+            temp.at<Vec3b>(i, j) = image.at<Vec3b>(i+pmed.x,j);
+            image.at<Vec3b>(i+pmed.x,j) = image.at<Vec3b>(i,j+pmed.y);
+            image.at<Vec3b>(i,j+pmed.y) = temp.at<Vec3b>(i, j);
+        }
     }
-  }
 
-  //Exibindo imagem:
-  imshow("Regioes trocadas", image);
-  waitKey();
-
-  return 0;
+    //Exibindo imagem:
+    imshow("Regioes trocadas", image);
+    imwrite("regioes_trocadas.jpg", image);
+    waitKey();
+    return 0;
 }
